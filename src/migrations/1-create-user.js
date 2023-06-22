3be97560df7b47d5b547c799b6074a238569e03a
thien <nguyenthanhthien0507@gmail.com>
2023-06-23 01:35:58 +0700

    update image default

--- src/migrations/1-create-user.js
+++ src/migrations/1-create-user.js
@@ -20,7 +20,7 @@ module.exports = {
       picture: {
         type: Sequelize.STRING(1000),
         defaultValue:
-          "https://firebasestorage.googleapis.com/v0/b/project1-ad195.appspot.com/o/thien%2Fdefault-image.png?alt=media&token=68763a44-9a68-4de7-827f-1bf4e9766672&_gl=1*hbiuj6*_ga*NDA0MDQxODkzLjE2ODIyNjA2Njc.*_ga_CW55HF8NVT*MTY4NjMyNDkxMC4xNS4xLjE2ODYzMjUwMzkuMC4wLjA.",
+          "https://firebasestorage.googleapis.com/v0/b/project1-ad195.appspot.com/o/thien%2Fdefaut.png?alt=media&token=64c2e4f5-923d-45ef-881e-d6adc1725317",
         allowNull: false,
       },
       role: {
